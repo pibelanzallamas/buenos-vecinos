e4ade49e28e9d97b17ea67db1552f027a754be3d
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-01-16 17:01:15 -0300

    welcome mobile

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 function Login() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("hoooa");
+  }
+
   return (
     <div className="content-page form-page">
       <h3>Iniciar Sesión</h3>
-      <form className="user-form">
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="name"></input>
-        <label htmlFor="password">Contraseña</label>
-        <input type="password" name="password" id="password"></input>
+      <form onSubmit={(e) => handleSubmit(e)} className="user-form">
+        <div className="input">
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="name" required></input>
+        </div>
+        <div className="input">
+          <label htmlFor="password">Contraseña:</label>
+          <input type="password" name="password" id="password" required></input>
+        </div>
+        <button type="submit">Entrar</button>
       </form>
+
+      <p>¿Necesita registrarse?</p>
     </div>
   );
 }
