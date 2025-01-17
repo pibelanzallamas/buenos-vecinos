a99aba67eb11e1e055c74846dd8431be88fad424
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-01-17 12:08:22 -0300

    welcome login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./styles/app.css";
 import AppRoutes from "./components/AppRoutes";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="main-container">
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MenuIcon from "../assets/Menu.svg";
+import X from "../assets/X.svg";
 
 function Navbar() {
+  const [menu, setMenu] = useState(false);
   const navigate = useNavigate();
+
+  function handleMenu() {
+    setMenu(!menu);
+  }
 
   return (
     <nav>
@@ -12,11 +18,24 @@ function Navbar() {
           <h3 onClick={() => navigate("/")}>Buenos Vecinos</h3>
         </li>
         <li>
-          <figure>
-            <img src={MenuIcon} alt="hamburger menu icon"></img>
-          </figure>
+          {menu ? (
+            <figure onClick={() => handleMenu()}>
+              <img src={X} alt="x menu icon"></img>
+            </figure>
+          ) : (
+            <figure onClick={() => handleMenu()}>
+              <img src={MenuIcon} alt="hamburger menu icon"></img>
+            </figure>
+          )}
         </li>
       </ul>
+      {menu && (
+        <ul className="mobile-menu">
+          <a href="#">Inicio</a>
+          <a href="#">Mapa</a>
+          <a href="#">Fecha Límite</a>
+        </ul>
+      )}
     </nav>
   );
 }
