555c93929bebf1ac37dd34736257ec3932db0a25
pibelanzallamas <brandoncastillo.09@gmail.com>
2025-01-21 08:59:53 -0300

    search bar mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../assets/Menu.svg";
 import Search from "../assets/Search.svg";
 
 function Home() {
+  const [menu, setMenu] = useState(false);
+
+  function handleSearch() {}
+
   return (
     <div className="content-page home-page">
       <section className="search">
         <h3>¿Qué está buscando?</h3>
         <div className="input-searcher">
-          <figure>
+          <figure onClick={() => setMenu(!menu)}>
             <img src={Menu} alt="menu icon" />
           </figure>
           <input placeholder="Bananas"></input>
-          <figure>
+          <figure onClick={() => handleSearch()}>
             <img src={Search} alt="search icon" />
           </figure>
         </div>
+        {menu && <div> </div>}
       </section>
     </div>
   );
